dcdc21dd63fae5cb8866f62202b7e667500ed768
parkcl <cparkp0wa@gmail.com>
2017-05-22 16:28:13 -0700

    setup, requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='lastfmpy',
+    version='0.0.1',
+    url='https://github.com/parkcl/lastfmpy',
+    author="@parkcl",
+    license='MIT',
+    install_requires=['requests']
+)
